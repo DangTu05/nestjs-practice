9c35d49cad5b399b872df3ddff38486ac62eae50
Dang Tu <dtu161205@gmail.com>
2025-08-09 14:18:17 +0700

    Kết nối vói mongodb và tạo schema user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const mongo_url: string = configService.get('MONGO_URL');
+        return {
+          uri: mongo_url,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
